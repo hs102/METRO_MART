6279deacf9806f27d3ac6d40e3efefe458149a7f
hs102 <herotato108@gmail.com>
2024-04-26 22:44:06 +0300

    MVC

---
+++ Views/navbar.php
@@ -0,0 +1,15 @@
+
+<nav>
+
+<link rel="stylesheet" type="text/css" href="./Styles/navbar.css">
+
+    <img src="./images/metroside.jpg" alt="Logo" style="float: left;">
+
+    <div class="buttons">
+    
+        <a href="loginFE.php">Login</a>
+        <a href="#">Sign Up</a>
+        
+    
+    </div>
+</nav>
